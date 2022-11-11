c97714ff9f5e66a6fe3fbc26ce334560ffbb0706
marcelacrvalho <marcelacrvalho@icloud.com>
2022-11-11 19:03:08 -0300

    Adiciona e implementa controller

---
+++ lib/modules/home/controllers/home_controller.dart
@@ -0,0 +1,20 @@
+import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
+
+class HomeController {
+  late String ticket;
+
+  Future<Object> readQRCode() async {
+    try {
+      String code = await FlutterBarcodeScanner.scanBarcode(
+        '#FFFFFF',
+        'Cancelar',
+        false,
+        ScanMode.QR,
+      );
+      return code != '-1' ? ticket = code : ticket = 'QR Code inválido';
+    } catch(e) {
+      return Exception('Problemas ao tentar ler o QR Code');
+    }
+  }
+
+}
